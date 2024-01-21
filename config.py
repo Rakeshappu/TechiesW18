206e00d8f8b6403d7d5d880e20f7bf5763915f29
Rakeshappu <appussrh@gmail.com>
2024-01-21 09:59:48 +0530

    Carbon Footprint

---
+++ config.py
@@ -0,0 +1,12 @@
+MYSQL_USER = 'root'
+MYSQL_HOST = 'localhost'
+MYSQL_PASSWORD = '1234'
+MYSQL_DB = 'flask'
+
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'appussrh@gmail.com'
+MAIL_PASSWORD = 'wdpbxdfyoxfkpelk'
+
+SECRET_KEY = 'some_secret'
